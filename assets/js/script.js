3d65cfac5856d5168fa8ba01caa6ad2568ed098b
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-02-17 16:16:33 -0500

    added a better js program

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ $("#foodForm").on("submit",function(event){
     event.preventDefault();
     var foodName = $("#inputSearch").val()
     $("#inputSearch").val("")
+    $("#mainContent").empty()
     if(!foodName){
         return
     }
@@ -12,47 +13,69 @@ $.ajax({
     method: "GET"
 }).then(function(response){
 
+     for(var r = 0; r<response.hits.length;r++){
+  
     var main= $("#mainContent")
         main.attr("style","border: groove; margin-bottom: 40px; padding:20px;")
     var foodCard = $("<div class='card mb-3 p-3'>")
+    var iframeEl = $("<iframe>") 
+    var sourceLink = response.hits[r].recipe.url
+        iframeEl.attr("width","755","height","400")
+        iframeEl.attr("src",response.hits[r].recipe.url)
+        console.log(sourceLink); 
     var foodRow1 =$("<div class='row'>")
     var foodCol1 = $("<div class='col-6'>")
     var foodCol2 = $("<div class='col-6'>")
     var foodTitle =$("<h5>")
-        foodTitle.text(response.hits[0].recipe.label)
-    var img = $("<img>")
-        img.attr("src",response.hits[0].recipe.image)
+        foodTitle.text(response.hits[r].recipe.label)
+    var img = $("<img class='img-fluid'>")
+        img.attr("src",response.hits[r].recipe.image)
+    var calEl = $("<div class= 'col-12 mt-3'>")
+        calEl.attr("style","border: groove;")
+        calEl.text("Calories: " + response.hits[r].recipe.calories.toFixed(0)+ " KCAL")
+    var fatEl = $("<p>")
+        fatEl.text("Fat: " + response.hits[r].recipe.totalNutrients.FAT.quantity.toFixed(0)+" G")
+    var protienEl =$("<p>")
+        protienEl.text("Protien: " + response.hits[r].recipe.totalNutrients.PROCNT.quantity.toFixed(0)+" G")
     var healthLabel = $("<ul>")
-        healthLabel.attr("sytle","border: dotted;")
+        healthLabel.attr("style","border: 5px solid black;")
         healthLabel.text("Health Label: ")
     var foodList =$("<ul>")
-        foodList.attr("style","border: dotted;")
+        foodList.attr("style","border: dotted; ")
         foodList.text("Ingredients: ")
+
     main.prepend(foodCard)
     foodCard.append(foodRow1)
     foodRow1.append(foodCol1)
     foodRow1.append(foodCol2)
     foodCol1.append(img)
+    foodCol1.append(calEl)
+    calEl.append(fatEl)
+    fatEl.append(protienEl)
     foodCol2.append(foodTitle)
     foodCol2.append(healthLabel)
     foodCol2.append(foodList)
+    foodCard.append(iframeEl)  
+ 
 
-    for(var h = 0; h<response.hits[0].recipe.healthLabels.length; h++){
+
+
+    for(var h = 0; h < response.hits[r].recipe.healthLabels.length; h++){
         var heaLabEl = $("<li>")
-            heaLabEl.text(response.hits[0].recipe.healthLabels[h])
+            heaLabEl.text(response.hits[r].recipe.healthLabels[h])
 
             healthLabel.append(heaLabEl)
     }
 
-    for(var i = 0; i < response.hits[0].recipe.ingredientLines.length; i++){
-    
+    for(var i = 0; i < response.hits[r].recipe.ingredientLines.length; i++){
         var ingEl = $("<li>")
-            ingEl.text(response.hits[0].recipe.ingredientLines[i])
-   
+            ingEl.text(response.hits[r].recipe.ingredientLines[i])
             foodList.append(ingEl)
 
  } 
- 
+
+}
+
 });
 
 });
