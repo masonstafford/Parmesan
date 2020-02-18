51d031c10cae64c0a42d8cd292ddb09c59a40bbb
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-02-18 10:37:47 -0500

    idk

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,37 +12,35 @@ $.ajax({
     url: queryUrl,
     method: "GET"
 }).then(function(response){
-
-     for(var r = 0; r<response.hits.length;r++){
-  
+// a for loop is placed so it will generate all of the recepies in the 
     var main= $("#mainContent")
         main.attr("style","border: groove; margin-bottom: 40px; padding:20px;")
-    var foodCard = $("<div class='card mb-3 p-3'>")
-    var iframeEl = $("<iframe>") 
-    var sourceLink = response.hits[r].recipe.url
-        iframeEl.attr("width","755","height","400")
-        iframeEl.attr("src",response.hits[r].recipe.url)
-        console.log(sourceLink); 
-    var foodRow1 =$("<div class='row'>")
+    var foodCard = $("<div class='card'>")
+    // mb-3 p-3
+   
+    var foodRow1 =$("<div class='co'>")
     var foodCol1 = $("<div class='col-6'>")
     var foodCol2 = $("<div class='col-6'>")
-    var foodTitle =$("<h5>")
-        foodTitle.text(response.hits[r].recipe.label)
+    var foodTitle =$("<h5>")  
+        foodTitle.text(response.hits[0].recipe.label)
     var img = $("<img class='img-fluid'>")
-        img.attr("src",response.hits[r].recipe.image)
+        img.attr("src",response.hits[0].recipe.image)
     var calEl = $("<div class= 'col-12 mt-3'>")
         calEl.attr("style","border: groove;")
-        calEl.text("Calories: " + response.hits[r].recipe.calories.toFixed(0)+ " KCAL")
+        calEl.text("Calories: " + response.hits[0].recipe.calories.toFixed(0)+ " KCAL")
     var fatEl = $("<p>")
-        fatEl.text("Fat: " + response.hits[r].recipe.totalNutrients.FAT.quantity.toFixed(0)+" G")
+        fatEl.text("Fat: " + response.hits[0].recipe.totalNutrients.FAT.quantity.toFixed(0)+" G")
     var protienEl =$("<p>")
-        protienEl.text("Protien: " + response.hits[r].recipe.totalNutrients.PROCNT.quantity.toFixed(0)+" G")
+        protienEl.text("Protien: " + response.hits[0].recipe.totalNutrients.PROCNT.quantity.toFixed(0)+" G")
     var healthLabel = $("<ul>")
         healthLabel.attr("style","border: 5px solid black;")
         healthLabel.text("Health Label: ")
     var foodList =$("<ul>")
         foodList.attr("style","border: dotted; ")
-        foodList.text("Ingredients: ")
+        foodList.text("Ingredients: ") 
+    var iframeEl = $("<iframe>") 
+        iframeEl.attr("width","755","height","400")
+        iframeEl.attr("src",response.hits[0].recipe.url)
 
     main.prepend(foodCard)
     foodCard.append(foodRow1)
@@ -60,19 +58,17 @@ $.ajax({
 
 
 
-    for(var h = 0; h < response.hits[r].recipe.healthLabels.length; h++){
+    for(var h = 0; h < response.hits[0].recipe.healthLabels.length; h++){
         var heaLabEl = $("<li>")
-            heaLabEl.text(response.hits[r].recipe.healthLabels[h])
+            heaLabEl.text(response.hits[0].recipe.healthLabels[h])
 
             healthLabel.append(heaLabEl)
     }
 
-    for(var i = 0; i < response.hits[r].recipe.ingredientLines.length; i++){
+    for(var i = 0; i < response.hits[0].recipe.ingredientLines.length; i++){
         var ingEl = $("<li>")
-            ingEl.text(response.hits[r].recipe.ingredientLines[i])
+            ingEl.text(response.hits[0].recipe.ingredientLines[i])
             foodList.append(ingEl)
-
- } 
 
 }
 
